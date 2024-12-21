3d0aa1d2d507e05ccb9d73132937a53860c4ebcf
QuiQuang <huyhoangdinhcong2003@gamil.com>
2024-12-21 19:00:07 +0700

    Fix

--- modules/GPU/nnGPU.h
+++ modules/GPU/nnGPU.h
@@ -1,12 +1,7 @@
 #ifndef NEURAL_NETWORK_GPU_H
 #define NEURAL_NETWORK_GPU_H
 
-#include <cfloat>
-#include <cmath>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include "../../utils/utils.h"
+#include "../nnCPU/nnCPU.h"
 
 returnStruct trainKernel1(unsigned char **trainImages, unsigned char *trainLabels, unsigned char **testImages, unsigned char *testLabels, int numTrainImages, int numTestImages, int numRows, int numCols, int numEpochs);
 
--- modules/OptimizeV2/nnO2.h
+++ modules/OptimizeV2/nnO2.h
@@ -1,12 +1,7 @@
 #ifndef NEURAL_NETWORK_OP2_H
 #define NEURAL_NETWORK_OP2_H
 
-#include <cfloat>
-#include <cmath>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include "../../utils/utils.h"
+#include "../nnCPU/nnCPU.h"
 
 returnStruct trainKernel3(unsigned char **trainImages, unsigned char *trainLabels, unsigned char **testImages, unsigned char *testLabels, int numTrainImages, int numTestImages, int numRows, int numCols, int numEpochs);
 
